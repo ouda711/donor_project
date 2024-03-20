4375394c14401e2f58773474920b81ebb769dada
Ouda Wycliffe <ouda.wycliffe.com>
2024-03-20 17:51:35 +0300

    fix for projects

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Host, Connect, Track: Your Projects, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Get to post projects and display them for visibility.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">Featured Projects</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
@@ -53,7 +53,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
 
         <Collection 
           data={events?.data}
-          emptyTitle="No Events Found"
+          emptyTitle="No Projects Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={6}
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'pTraccar',
+  description: 'pTraccar is a platform for project progress tracking.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
